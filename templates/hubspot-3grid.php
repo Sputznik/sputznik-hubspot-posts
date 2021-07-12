7295c9cdc88aa14f01102efa285fdadf3827acce
psychome004 <anil.stephen66@gmail.com>
2021-07-12 11:33:59 +0530

    Open blog-post on new tab

--- templates/hubspot-3grid.php
+++ templates/hubspot-3grid.php
@@ -6,7 +6,7 @@
       $title = $slug->htmlTitle
     ?>
     <li class="sp-hs-post">
-      <a class="hubspot-post-wrapper" href="<?php if( $url && !empty( $url ) ){ _e( $url ); } else{ echo "#"; }?>">
+      <a class="hubspot-post-wrapper" href="<?php if( $url && !empty( $url ) ){ _e( $url ); } else{ echo "#"; }?>" target="_blank" rel="noopener noreferrer">
         <div class="thumbnail-bg" <?php if( $featured_image && !empty( $featured_image ) ){ echo 'style="background-image:url('.$featured_image.');"'; }?>></div>
         <div class="title"><?php if( $title && !empty( $title ) ){ _e( $title ); }?></div>
       </a>
